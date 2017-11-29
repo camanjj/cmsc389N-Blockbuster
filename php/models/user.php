08cf9be0e5e1801fa5d5bdfc0f4842e6f44401da
Cameron Jackson <cjack82@terpmail.umd.edu>
2017-11-29 16:07:34 -0500

    tweak user static methods

--- php/models/user.php
+++ php/models/user.php
@@ -2,51 +2,58 @@
 
 require_once "../dbLogin.php";
 
-class User {
+class User
+{
 
-  public $username;
+    public $username;
 
-  static function login(string $username, string $password) : int {
-    // check database
-    global $db_connection;
+    static function login(string $username, string $password): int
+    {
+        // check database
+        global $db_connection;
 
-    $password = password_hash($password, PASSWORD_DEFAULT);
+        $password = password_hash($password, PASSWORD_DEFAULT);
 
-    $result = $db_connection->query("select * from user where username=$username and password=$password");
-    $user_row = mysqli_fetch_row($result);
+        $result = $db_connection->query("select * from User where username=$username and password=$password");
+        $user_row = mysqli_fetch_row($result);
 
-    if (mysqli_num_rows($result) != 1) {
-        return -1;
+        if (mysqli_num_rows($result) != 1) {
+            return -1;
+        }
+
+        $result->free();
+
+        return $user_row[0];
     }
 
-    return $user_row[0];
-  }
+    static function register(string $username, string $password): int
+    {
+        // check if username was taken, if not create the user
+        global $db_connection;
 
-  static function register(string $username, string $password) : int {
-    // check if username was taken, if not create the user
-    global $db_connection;
+        // check if the username was taken
+        $query = $db_connection->query("select * from User where username=$username");
+        $count = mysqli_num_rows($query);
 
-    // check if the username was taken
-    $query = $db_connection->query("select * from user where username=$username");
-    $count = mysqli_num_rows($query);
+        if ($count > 0) {
+            // username already taken
+            return -1;
+        }
 
-    if ($count > 0) {
-      // username already taken
-      return -1;
+        // hash the password
+        $password = password_hash($password, PASSWORD_DEFAULT);
+
+        $result = $db_connection->query("insert into User (username, password) values ($username, $password)");
+        if (mysqli_num_rows($result) != 1) {
+            return -1;
+        }
+
+        $user_row = mysqli_fetch_row($result);
+        // create the default libraries for the user
+
+        $result->free();
+
+        return $user_row[0];
     }
-
-    // hash the password
-    $password = password_hash($password, PASSWORD_DEFAULT);
-
-    $result = $db_connection->query("insert into user (username, password) values ($username, $password)");
-    if (mysqli_num_rows($result) != 1) {
-        return -1;
-    }
-
-    $user_row = mysqli_fetch_row($result);
-    // create the default libraries for the user
-
-    return $user_row[0];
-  }
 
 }
