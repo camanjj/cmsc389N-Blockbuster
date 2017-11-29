499d2c8dd404ef64e96a48a1151971105a04249a
Cameron Jackson <cjack82@terpmail.umd.edu>
2017-11-29 16:07:08 -0500

    add port to host in db login file

--- php/dbLogin.php
+++ php/dbLogin.php
@@ -1,10 +1,10 @@
 <?
 
-	/* Update based on your database and account info */
-	$host = "localhost";
-	$user = "cmsc389n";
-	$password = "blockbuster";
-	$database = "blockbuster";
+/* Update based on your database and account info */
+$host = "localhost:3306";
+$user = "cmsc389n";
+$password = "blockbuster";
+$database = "blockbuster";
 
 $db_connection = new mysqli($host, $user, $password, $database);
 if ($db_connection->connect_error) {
