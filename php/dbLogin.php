148363c0518ef923bdb7c7fe8bbddfc8ff4c421c
Cameron Jackson <cjack82@terpmail.umd.edu>
2017-11-29 14:36:17 -0500

    add dblogin class

---
+++ php/dbLogin.php
@@ -0,0 +1,14 @@
+<?
+
+	/* Update based on your database and account info */
+	$host = "localhost";
+	$user = "cmsc389n";
+	$password = "blockbuster";
+	$database = "blockbuster";
+
+$db_connection = new mysqli($host, $user, $password, $database);
+if ($db_connection->connect_error) {
+    die($db_connection->connect_error);
+} else {
+//    echo "Connection to database established<br><br>";
+}
